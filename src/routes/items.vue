72fb56f722ff9f5ea10e4f6f60911e2d9bb99b6b
bereikme <info@bereikme.nl>
2020-06-22 14:29:30 +0200

    Update items.vue

--- src/routes/items.vue
+++ src/routes/items.vue
@@ -1,7 +1,16 @@
 <template>
 	<v-not-found v-if="notFound" />
 	<div v-else class="route-item-listing">
-	
+		<v-header
+			info-toggle
+			:item-detail="false"
+			:icon="breadcrumbIcon"
+			:settings="collection === 'directus_webhooks'"
+			:title="currentBookmark && currentBookmark.title"
+			:icon-link="
+				collection === 'directus_webhooks' ? `/${currentProjectKey}/settings/` : null
+			"
+		>
 			<template slot="title">
 				<button
 					:class="currentBookmark ? 'active' : null"
