fecd22f2565a8455a836f6ffab2bde33574afc67
bereikme <info@bereikme.nl>
2020-06-25 23:17:53 +0200

    Update dashboard.vue

--- src/routes/dashboard.vue
+++ src/routes/dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="settings">
+	<div class="settings-global">
 		<v-header :breadcrumb="links" :icon-link="`/${currentProjectKey}/settings`" settings>
 			<template slot="buttons">
 				<v-header-button
@@ -13,50 +13,6 @@
 				/>
 			</template>
 		</v-header>
-		
-		<v-details :title="$t('settings_project')" type="break" open>
-			<nav>
-				<ul>
-					<v-card
-						:title="$t('settings_global')"
-						element="li"
-						:to="`/${currentProjectKey}/settings/global`"
-						icon="public"
-					/>
-					
-					<v-card
-						:title="$t('settings_default')"
-					element="li"
-						:to="`/${currentProjectKey}/settings/default`"
-						icon="public"
-					/>
-
-					<v-card
-						:title="$t('settings_collections_fields')"
-					
-						element="li"
-						:to="`/${currentProjectKey}/settings/collections`"
-						icon="box"
-					/>
-
-					<v-card
-						:title="$t('settings_permissions')"
-						:subtitle="roleCount"
-						element="li"
-						:to="`/${currentProjectKey}/settings/roles`"
-						icon="group"
-					/>
-
-					<v-card
-						:title="$t('settings_webhooks')"
-						:subtitle="webhookCount"
-						element="li"
-						:to="`/${currentProjectKey}/settings/webhooks`"
-						icon="send"
-					/>
-				</ul>
-			</nav>
-		</v-details>
 		
 		<v-details :title="$t('settings_project')" type="break" open>
 			<nav>
@@ -221,5 +177,4 @@ nav ul {
 .signal {
 	fill: var(--white);
 }
-
 </style>
