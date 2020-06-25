cd210d1606b3892927f5e0607e541c6be18f3b02
bereikme <info@bereikme.nl>
2020-06-25 22:23:29 +0200

    Update dashboard.vue

--- src/routes/dashboard.vue
+++ src/routes/dashboard.vue
@@ -1,25 +1,88 @@
 <template>
-	<div class="settings-global">
-		<v-header :breadcrumb="links" :icon-link="`/${currentProjectKey}/settings`" settings>
-			<template slot="buttons">
-				<v-header-button
-					:disabled="!editing"
-					:loading="saving"
-					:label="$t('save')"
-					icon="check" 
-					background-color="button-primary-background-color"
-					icon-color="button-primary-text-color"
-					@click="save('leave')"
-				/>
-			</template>
-		</v-header>
+	<div class="settings">
+		<v-header :breadcrumb="links" icon="home" dashboard /> 
 
-		<v-form
-			:fields="fields"
-			:values="values"
-			collection="directus_settings"
-			@stage-value="stageValue"
-		/>
+		<v-details :title="$t('dashboard_news')" type="break" open>
+			<nav>
+				<ul>
+					<v-card
+						:title="$t('settings_global')"
+						:subtitle="$tc('item_count', globalNum, { count: globalNum })"
+						element="li"
+						:to="`/${currentProjectKey}/settings/global`"
+						icon="public"
+					/>
+					
+					<v-card
+						:title="$t('settings_default')"
+						:subtitle="$tc('item_count', globalNum, { count: globalNum })"
+						element="li"
+						:to="`/${currentProjectKey}/settings/default`"
+						icon="public"
+					/>
+
+					<v-card
+						:title="$t('settings_collections_fields')"
+						:subtitle="
+							$tc('collection_count', collectionsNum, { count: collectionsNum })
+						"
+						element="li"
+						:to="`/${currentProjectKey}/settings/collections`"
+						icon="box"
+					/>
+
+					<v-card
+						:title="$t('settings_permissions')"
+						:subtitle="roleCount"
+						element="li"
+						:to="`/${currentProjectKey}/settings/roles`"
+						icon="group"
+					/>
+
+					<v-card
+						:title="$t('settings_webhooks')"
+						:subtitle="webhookCount"
+						element="li"
+						:to="`/${currentProjectKey}/settings/webhooks`"
+						icon="send"
+					/>
+				</ul>
+			</nav>
+		</v-details>
+		
+		<v-details :title="$t('dashboard_news')" type="break" open>
+			<nav>
+				<ul>
+					<v-card
+						:title="$t('dashboard_view_invoices')"
+						:subtitle="$tc('item_count', globalNum, { count: globalNum })"
+						element="li"
+						:to="`/${currentProjectKey}/settings/global`"
+						icon="public"
+					/>
+					
+					<v-card
+						:title="$t('dashboard_view_balance')"
+						:subtitle="$tc('item_count', globalNum, { count: globalNum })"
+						element="li"
+						:to="`/${currentProjectKey}/settings/default`"
+						icon="public"
+					/>
+
+					<v-card
+						:title="$t('dashboard_add_funds')"
+						:subtitle="
+							$tc('collection_count', collectionsNum, { count: collectionsNum })
+						"
+						element="li"
+						:to="`/${currentProjectKey}/settings/collections`"
+						icon="box"
+					/>
+				</ul>
+			</nav>
+		</v-details>
+	
+
 		<v-info-sidebar wide>
 			<span class="type-note">No settings</span>
 		</v-info-sidebar>
@@ -27,83 +90,110 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { version } from '../../../package.json';
+import VSignal from '../../components/signal.vue';
+import { mapGetters, mapState } from 'vuex';
 
 export default {
-	name: 'SettingsGlobal',
+	name: 'Settings',
 	metaInfo() {
 		return {
-			title: `${this.$t('settings')} | ${this.$t('settings_global')}`
+			title: `${this.$t('settings')}`
 		};
+	},
+	components: {
+		VSignal
 	},
 	data() {
 		return {
-			saving: false,
-			edits: {}
+			roleCount: this.$t('loading'),
+			activityCount: this.$t('loading'),
+			webhookCount: this.$t('loading')
 		};
 	},
 	computed: {
-		...mapState({
-			settings: state => state.settings.values,
-			fields: state => state.collections.directus_settings.fields,
-			currentProjectKey: state => state.currentProjectKey
-		}),
-		values() {
-			return {
-				...this.settings,
-				...this.edits
-			};
+		...mapGetters(['currentProject']),
+		...mapState(['currentProjectKey']),
+		globalNum() {
+			return Object.keys(this.$store.state.collections.directus_settings.fields).length;
+		},
+		collectionsNum() {
+			return Object.keys(this.$store.state.collections).filter(
+				name => name.startsWith('directus_') === false
+			).length;
+		},
+		projectName() {
+			return this.currentProject.project_name;
+		},
+		interfaceCount() {
+			return Object.keys(this.$store.state.extensions.interfaces).length;
+		},
+		version() {
+			return version;
 		},
 		links() {
 			return [
 				{
 					name: this.$t('settings'),
 					path: `/${this.currentProjectKey}/settings`
-				},
-				{
-					name: this.$t('settings_global'),
-					path: `/${this.currentProjectKey}/settings/global`
 				}
 			];
-		},
-		editing() {
-			return Object.keys(this.edits).length > 0;
 		}
 	},
+	created() {
+		this.getRoleCount();
+		this.getActivityCount();
+		this.getWebhookCount();
+	},
 	methods: {
-		stageValue({ field, value }) {
-			if (this.settings[field] == value) {
-				return this.$delete(this.edits, field);
-			}
-
-			return this.$set(this.edits, field, value);
-		},
-		save() {
-			this.saving = true;
-
-			this.$store
-				.dispatch('setSettings', this.edits)
-				.then(() => {
-					this.saving = false;
-					this.edits = {};
-
-					// Update the current project's info in the store when saving settings, this makes sure
-					// the logo / name in the top left etc will update
-					this.$store.dispatch('updateProjectInfo', this.currentProjectKey);
-
-					this.$router.push(`/${this.currentProjectKey}/settings`);
-					this.$notify({
-						title: this.$t('settings_saved'),
-						color: 'green',
-						iconMain: 'check'
+		getRoleCount() {
+			this.$api
+				.getItems('directus_roles', {
+					fields: '-',
+					limit: 0,
+					meta: 'total_count' 
+				})
+				.then(res => res.meta)
+				.then(({ total_count }) => {
+					this.roleCount = this.$tc('role_count', total_count, {
+						count: this.$n(total_count)
 					});
 				})
-				.catch(error => {
-					this.saving = false;
-					this.$events.emit('error', {
-						notify: error.message || this.$t('something_went_wrong_body'),
-						error
+				.catch(() => {
+					this.roleCount = '--';
+				});
+		},
+		getActivityCount() {
+			this.$api
+				.getItems('directus_activity', {
+					fields: '-',
+					limit: 0,
+					meta: 'total_count'
+				})
+				.then(res => res.meta)
+				.then(({ total_count }) => {
+					this.activityCount = this.$tc('event_count', total_count, {
+						count: this.$n(total_count)
 					});
+				})
+				.catch(() => {
+					this.activityCount = '--';
+				});
+		},
+		getWebhookCount() {
+			this.$api
+				.getItems('directus_webhooks', {
+					limit: 0,
+					meta: 'total_count'
+				})
+				.then(res => res.meta)
+				.then(({ total_count }) => {
+					this.webhookCount = this.$tc('webhook_count', total_count, {
+						count: this.$n(total_count)
+					});
+				})
+				.catch(() => {
+					this.webhookCount = '--';
 				});
 		}
 	}
@@ -111,7 +201,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.settings-global {
+.settings {
 	padding: var(--page-padding-top) var(--page-padding) var(--page-padding-bottom);
+}
+
+nav ul {
+	padding: 0;
+	display: grid;
+	grid-template-columns: repeat(auto-fill, var(--card-size));
+	grid-gap: var(--card-horizontal-gap);
+
+	li {
+		display: block;
+	}
+}
+
+.signal {
+	fill: var(--white);
 }
 </style>
