11c7ac61d2b4a3f4e43702ab3dacf92b0a1b91ef
bereikme <info@bereikme.nl>
2020-07-01 16:32:52 +0200

    Update register.vue

--- src/routes/register.vue
+++ src/routes/register.vue
@@ -169,7 +169,7 @@ export default {
      
 			try {
 				await axios({
-				  url: `http://104.248.94.149/works/put_users.php?first_name=${first_name}&last_name=${last_name}&email=${user_email}&password=${user_password}`,
+				  url: `http://104.248.94.149/works/put_users.php?first_name=${first_name}&last_name=${last_name}&email=${user_email}&password=${user_password}`, 
 				  method: 'post'
 				})
 				
