d701cd951b1874952f288257d9ad8bd3c6d0b703
bereikme <info@bereikme.nl>
2020-07-01 15:30:23 +0200

    Update register.vue

--- src/routes/register.vue
+++ src/routes/register.vue
@@ -168,8 +168,8 @@ export default {
 
 			try {
 				await axios({
-				  url: 'http://104.248.94.149/works/put_users.php?first_name=john&last_name=doe&email=info@abrazozo.com&password=Fietsbel10',
-				  method: 'get'
+				  url: 'http://(thisismyip)/works/put_users.php?first_name=${first_name}&last_name=${last_name}&email=${user_email}&password=${user_password}',
+				  method: 'post'
 				})
 				
 			} catch (error) {
