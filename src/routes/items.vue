e79c98ab902b239f88962c56099f0344c76d961f
bereikme <info@bereikme.nl>
2020-07-02 14:36:43 +0200

    Update items.vue

--- src/routes/items.vue
+++ src/routes/items.vue
@@ -72,6 +72,15 @@
 					:label="$t('new')"
 					:to="createLink"
 				/>
+				<v-header-button
+					v-if="addButton && !activity"
+					key="order"
+					icon="shopping_cart"
+					icon-color="button-primary-text-color"
+					background-color="button-primary-background-color"
+					:label="$t('new')"
+					:to="createLink"
+				/>
 			</template>
 		</v-header>
 
